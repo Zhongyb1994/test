55a6d77c952cf2b5d78ec9290497311453a5d533
wengyuzhu <wangyz@fshows.com>
2019-09-10 20:17:04 +0800

    简单service

---
+++ check-oeder-service/src/main/java/com/yutian/service/DemoService.java
@@ -0,0 +1,13 @@
+/**
+ * fshows.com
+ * Copyright (C) 2013-2019All Rights Reserved.
+ */
+package com.yutian.service;
+
+/**
+ *
+ * @author wangyz
+ * @version DemoService.java, v 0.1 2019-09-10 20:15
+ */
+public interface DemoService {
+}
---
+++ check-oeder-service/src/main/java/com/yutian/service/impl/DemoServiceImpl.java
@@ -0,0 +1,15 @@
+/**
+ * fshows.com
+ * Copyright (C) 2013-2019 All Rights Reserved.
+ */
+package com.yutian.service.impl;
+
+import com.yutian.service.DemoService;
+
+/**
+ *
+ * @author wangyz
+ * @version DemoServiceImpl.java, v 0.1 2019-09-10 20:16
+ */
+public class DemoServiceImpl implements DemoService {
+}
