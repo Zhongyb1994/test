0e692a86b2dd32673a5f8b28d929de98db580565
wengyuzhu <wangyz@fshows.com>
2019-09-19 20:45:19 +0800

    添加描述

--- check-order-controller/src/main/java/com/yutain/checkOrderApplication.java
+++ check-order-controller/src/main/java/com/yutain/checkOrderApplication.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
